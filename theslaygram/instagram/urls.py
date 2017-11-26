f60c0e4cd5c4722008b464ce01a57d8f116b4b7a
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-26 20:18:05 +0300

    Add update user profile url

--- theslaygram/instagram/urls.py
+++ theslaygram/instagram/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.index, name='slaygram')
+    url('^$', views.index, name='slaygram'),
+    url('^profiles/edit', views.update_user_profile, name="updateuserprofile")
 ]
