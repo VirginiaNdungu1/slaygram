5da4e11274fd3a66309990752348a55fd66a5e95
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-26 20:17:48 +0300

    Add update user profile view

--- theslaygram/instagram/views.py
+++ theslaygram/instagram/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.contrib import messages
+from .forms import UserProfileForm, UserForm
+from .models import Profile
 
 
 # Create your views here.
@@ -7,3 +11,27 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required
+@transaction.atomic
+def update_user_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        user_profile = UserProfileForm(
+            request.POST, instance=request.user.profile)
+
+        if user_form.is_valid() and user_profile.is_valid():
+            user_form.save()
+            user_profile.save()
+            messages.success(request, 'Profile successfully updated')
+            # return redirect('index')
+        else:
+            messages.error(
+                request, 'Error occured while updating,,, try again')
+
+    else:
+        user_form = UserForm(instance=request.user)
+        user_profile = UserProfileForm(instance=request.user.profile)
+
+    return render(request, 'profiles/profile.html', {'user_form': user_form, 'user_profile': user_profile, })
