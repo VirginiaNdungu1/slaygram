3e3ed62325f358e1d40971c4519760a3e85a47c2
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-26 14:22:19 +0300

    include django registration to our django-project url

--- theslaygram/theslaygram/urls.py
+++ theslaygram/theslaygram/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('instagram.urls'))
+    url(r'', include('instagram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 ]
