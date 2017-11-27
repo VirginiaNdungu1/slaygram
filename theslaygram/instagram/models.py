c78bd3438b8dc2a68f631e20fbaf34676f990b16
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-27 05:10:18 +0300

    Update Models

--- theslaygram/instagram/models.py
+++ theslaygram/instagram/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.db.models import Q, signals
 from django.dispatch import receiver
 from tinymce.models import HTMLField
+from vote.models import VoteModel
 # Create your models here.
 Gender_Choices = (
     ('F', 'female'),
@@ -50,8 +51,15 @@ class Post(models.Model):
     class Meta:
         ordering = ['pub_date']
 
+    # votes = VotableManager()
+
     def save_post(self):
         self.save()
+
+    @classmethod
+    def display_users_posts(cls, id):
+        posts = cls.objects.filter(user_id=id)
+        return posts
 
     @classmethod
     def display_posts(cls):
@@ -62,3 +70,18 @@ class Post(models.Model):
     def image_url(self):
         if self.image and hasattr(self.image, 'url'):
             return self.image.url
+
+
+class Review(models.Model):
+    users = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="vote")
+    pictures = models.ForeignKey(Post, related_name="vote")
+    liked = models.BooleanField(default=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comments = models.CharField(max_length=140, blank=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def save_review(self):
+        self.save()
