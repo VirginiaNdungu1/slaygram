e5e13145b04b5126995fcd0e3217bc796fb64e62
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-27 05:09:27 +0300

    Forms and update navbar

--- theslaygram/instagram/forms.py
+++ theslaygram/instagram/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile, User, Post
+from .models import Profile, User, Post, Review
 
 
 class UserForm(forms.ModelForm):
@@ -18,3 +18,9 @@ class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['user', 'pub_date']
+
+
+class NewReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ['users', 'pictures', 'liked', 'created_at']
