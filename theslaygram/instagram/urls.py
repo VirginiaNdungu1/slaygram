ac2995e08364bab5469f108f47bea91a529b4086
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-30 18:26:03 +0300

    voting urls

--- theslaygram/instagram/urls.py
+++ theslaygram/instagram/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url('^profiles/edit', views.update_user_profile, name="updateuserprofile"),
     url(r'^upload/new_post', views.new_post, name="new_post"),
     url(r'^profiles/(\d+)/$', views.posts, name="post"),
+    url(r'^post/upvote/(\d+)$', views.upvote_post, name="upvote_post"),
+    url(r'^post/downvote/(\d+)$', views.downvote_post, name="downvote_post")
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
