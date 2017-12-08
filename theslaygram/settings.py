3a7fad6473439eb88cc17f6f1f0eff3a75930652
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-08 12:56:08 +0300

    deploy to heroku

--- theslaygram/settings.py
+++ theslaygram/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['slaygram.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['slaygram.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
